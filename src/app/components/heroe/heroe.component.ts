e1ba719891d386c3d67e5af305f09a5983ef0965
nicolas <nicolasbloj@gmail.com>
2017-09-15 10:25:19 -0300

    clase56

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,25 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent implements OnInit {
+
+  heroe: Heroe ; // interface no puede tener constructor {} ?  . aqui un problema
+
+  constructor(private _activatedRoute: ActivatedRoute, private _heroesService: HeroesService) {
+    this._activatedRoute.params.subscribe((params) => {
+      console.log('Params:id -> ' + params['id']);
+      this.heroe = _heroesService.getHeroe(params['id']);
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService, private _router: Router) {
+    console.log('constructor');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx: number) {
+    console.log('index -> ' + idx);
+
+    this._router.navigate(['/heroe', idx]);
+
+  }
+
+}
